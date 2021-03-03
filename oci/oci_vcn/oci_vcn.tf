df409c6c88da88051a944f77d6993037ef2a430e
Tim Hall <tim@oracle-base.com>
2021-03-03 22:19:13 +0000

    Switch to using the vcn module, rather than the oci_core_vcn resource, as the oci_core_vcn resource seems to have problems with free tier accounts.

--- oci/oci_vcn/oci_vcn.tf
+++ oci/oci_vcn/oci_vcn.tf
@@ -1,49 +1,220 @@
 # Variables
 variable "compartment_id"   { type = string }
 variable "vcn_display_name" { type = string }
+variable "vcn_dns_label"    { type = string }
 
-variable "cidr_blocks" {
-  type    = list(string)
-  default = ["10.0.0.0/16"]
+variable "vnc_cidr_block" {
+  type    = string
+  default = "10.0.0.0/16"
 }
 
-variable "subnet_cidr_block" {
+variable "vnc_private_subnet_cidr_block" {
+  type    = string
+  default = "10.0.1.0/24"
+}
+
+variable "vnc_public_subnet_cidr_block" {
   type    = string
   default = "10.0.0.0/24"
 }
 
-variable "subnet_display_name" { type = string }
 
+# Modules and Resources
+module "vcn"{
+  source                   = "oracle-terraform-modules/vcn/oci"
+  version                  = "2.0.0"
+  
+  # Required
+  compartment_id           = var.compartment_id
+  region                   = var.region
+  vcn_name                 = var.vcn_display_name
+  vcn_dns_label            = var.vcn_dns_label
 
-# Resources
-resource "oci_core_vcn" "tf_vcn" {
-  compartment_id = var.compartment_id
-  cidr_blocks    = var.cidr_blocks
-  display_name   = var.vcn_display_name
+  # Optional
+  internet_gateway_enabled = true
+  # Commented out for my free tier account.
+  #nat_gateway_enabled      = true
+  #service_gateway_enabled  = true
+  vcn_cidr                 = var.vnc_cidr_block
 }
 
-resource "oci_core_subnet" "tf_subnet" {
-  cidr_block        = var.subnet_cidr_block
+resource "oci_core_subnet" "tf_vcn_private_subnet"{
+  # Required
   compartment_id    = var.compartment_id
-  vcn_id            = oci_core_vcn.tf_vcn.id
-  display_name      = var.subnet_display_name
-  security_list_ids = [oci_core_vcn.tf_vcn.default_security_list_id]
+  vcn_id            = module.vcn.vcn_id
+  cidr_block        = var.vnc_private_subnet_cidr_block
+
+  # Optional
+  route_table_id    = module.vcn.nat_route_id
+  security_list_ids = [oci_core_security_list.tf_private_security_list.id]
+  display_name      = "private-subnet"
 }
+
+resource "oci_core_subnet" "tf_vcn_public_subnet"{
+  # Required
+  compartment_id    = var.compartment_id
+  vcn_id            = module.vcn.vcn_id
+  cidr_block        = var.vnc_public_subnet_cidr_block
+
+  # Optional
+  route_table_id    = module.vcn.ig_route_id
+  security_list_ids = [oci_core_security_list.tf_public_security_list.id]
+  display_name      = "public-subnet"
+}
+
+resource "oci_core_security_list" "tf_private_security_list"{
+  compartment_id = var.compartment_id
+  vcn_id         = module.vcn.vcn_id
+  display_name   = "security-list-for-private-subnet"
+
+  egress_security_rules {
+    stateless        = false
+    destination      = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+    protocol         = "all" 
+  }
+
+  ingress_security_rules { 
+    stateless   = false
+    source      = var.vnc_cidr_block
+    source_type = "CIDR_BLOCK"
+
+    # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml TCP is 6
+    protocol    = "6"
+    tcp_options { 
+      min = 22
+      max = 22
+    }
+  }
+
+  ingress_security_rules { 
+    stateless   = false
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml ICMP is 1  
+    protocol    = "1"
+
+    # For ICMP type and code see: https://www.iana.org/assignments/icmp-parameters/icmp-parameters.xhtml
+    icmp_options {
+      type = 3
+      code = 4
+    } 
+  }   
+  
+  ingress_security_rules { 
+    stateless   = false
+    source      = var.vnc_cidr_block
+    source_type = "CIDR_BLOCK"
+    # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml ICMP is 1  
+    protocol    = "1"
+
+    # For ICMP type and code see: https://www.iana.org/assignments/icmp-parameters/icmp-parameters.xhtml
+    icmp_options {
+      type = 3
+    } 
+  }
+}
+
+resource "oci_core_security_list" "tf_public_security_list"{
+  compartment_id = var.compartment_id
+  vcn_id         = module.vcn.vcn_id
+  display_name   = "security-list-for-public-subnet"
+
+  egress_security_rules {
+    stateless        = false
+    destination      = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+    protocol         = "all" 
+  }
+
+  ingress_security_rules { 
+    stateless   = false
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml TCP is 6
+    protocol    = "6"
+    tcp_options { 
+        min = 22
+        max = 22
+    }
+  }
+
+  ingress_security_rules { 
+    stateless   = false
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml TCP is 6
+    protocol    = "6"
+    tcp_options { 
+        min = 1521
+        max = 1522
+    }
+  }
+
+  ingress_security_rules { 
+    stateless   = false
+    source      = "0.0.0.0/0"
+    source_type = "CIDR_BLOCK"
+    # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml ICMP is 1  
+    protocol    = "1"
+
+    # For ICMP type and code see: https://www.iana.org/assignments/icmp-parameters/icmp-parameters.xhtml
+    icmp_options {
+      type = 3
+      code = 4
+    } 
+  }   
+  
+  ingress_security_rules { 
+    stateless   = false
+    source      = var.vnc_cidr_block
+    source_type = "CIDR_BLOCK"
+    # Get protocol numbers from https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml ICMP is 1  
+    protocol    = "1"
+
+    # For ICMP type and code see: https://www.iana.org/assignments/icmp-parameters/icmp-parameters.xhtml
+    icmp_options {
+      type = 3
+    } 
+  }
+
+}
+
+resource "oci_core_dhcp_options" "tf_dhcp_options"{
+  # Required
+  compartment_id = var.compartment_id
+  vcn_id         = module.vcn.vcn_id
+
+  #Options for type are either "DomainNameServer" or "SearchDomain"
+  options {
+    type        = "DomainNameServer"  
+    server_type = "VcnLocalPlusInternet"
+  }
+  
+  # Optional
+  display_name = "default-dhcp-options"
+}
+
+
 
 
 # Outputs
-output "vcn_name" {
-  value = oci_core_vcn.tf_vcn.display_name
-}
-
 output "vcn_id" {
-  value = oci_core_vcn.tf_vcn.id
+  value = module.vcn.vcn_id
 }
 
-output "subnet_id" {
-  value = oci_core_subnet.tf_subnet.id
+output "private_security_list_id" {
+  value = oci_core_security_list.tf_private_security_list.id
 }
 
-output "default_security_list_id" {
-  value = oci_core_vcn.tf_vcn.default_security_list_id
+output "public_security_list_id" {
+  value = oci_core_security_list.tf_public_security_list.id
+}
+
+output "private_subnet_id" {
+  value = oci_core_subnet.tf_vcn_private_subnet.id
+}
+
+output "public_subnet_id" {
+  value = oci_core_subnet.tf_vcn_public_subnet.id
 }
